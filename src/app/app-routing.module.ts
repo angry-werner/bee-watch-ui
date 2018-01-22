dfd02ed23612af04adfa262b25ef7d4174b4dd1c
Werner Koch <werner.koch@bluewin.ch>
2018-01-22 21:19:37 +0100

    (kow) Linting.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LandingPageComponent } from "./shell/landing-page/landing-page.component";
+import { LandingPageComponent } from './shell/landing-page/landing-page.component';
 import { LocationsComponent } from './environment/locations/locations.component';
 
 const routes: Routes = [
